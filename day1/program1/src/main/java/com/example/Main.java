99646f3a7db75b9079b33725b235d09ef37b478c
DasunNethsara-04 <techsaralk.pro@gmail.com>
2026-03-08 10:01:40 +0530

    program1 added

---
+++ day1/program1/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // This is my first java program
+        System.out.println("Hello World!");
+        System.out.println("I'm learning Java!");
+    }
+}
