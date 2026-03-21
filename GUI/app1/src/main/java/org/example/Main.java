431ff4f9fdf7681ba5dc38c2b5aed7be2d214d78
DasunNethsara-04 <techsaralk.pro@gmail.com>
2026-03-21 20:29:40 +0530

    gui app 1: simple calculator

---
+++ GUI/app1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import ui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        });
+    }
+}
