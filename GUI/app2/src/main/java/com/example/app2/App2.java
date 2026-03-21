65d565537a33811b9993d8ffc37a7f9dee8b561a
DasunNethsara-04 <techsaralk.pro@gmail.com>
2026-03-21 20:57:54 +0530

    gui app 2: simple todo app

---
+++ GUI/app2/src/main/java/com/example/app2/App2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.example.app2;
+
+import ui.MainWindow;
+
+/**
+ *
+ * @author Dasun Nethsara
+ */
+public class App2 {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        });
+    }
+}
